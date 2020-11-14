d609fbf766389b5471ec943ed5d82f3b0d46b282
Gungnir Kiko <67295775+kiko612@users.noreply.github.com>
2020-11-14 13:48:55 +0800

    Update main_UI.py

--- main_UI.py
+++ main_UI.py
@@ -6,36 +6,27 @@ import cv2                      # 图像处理的库OpenCv
 import wx                       # 构造显示界面的GUI
 import wx.xrc
 import wx.adv
-# import the necessary packages
 from scipy.spatial import distance as dist
 from imutils.video import FileVideoStream
 from imutils.video import VideoStream
 from imutils import face_utils
-import numpy as np # 数据处理的库 numpy
+import numpy as np 
 import argparse
 import imutils
 import datetime,time
 import math
 import os
 
-###########################################################################
-## Class Fatigue_detecting
-###########################################################################
 
-COVER = 'D:/myworkspace/JupyterNotebook/fatigue_detecting/images/camera.png'
-
+COVER = './images/camera.png'
 class Fatigue_detecting(wx.Frame):
-
     def __init__( self, parent, title ):
-        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = title, pos = wx.DefaultPosition, size = wx.Size( 873,535 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
-                
+        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = title, pos = wx.DefaultPosition, size = wx.Size( 873,535 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL ) 
         self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
         self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_MENU ) )
-
         bSizer1 = wx.BoxSizer( wx.VERTICAL )
         bSizer2 = wx.BoxSizer( wx.HORIZONTAL )
         bSizer3 = wx.BoxSizer( wx.VERTICAL )
-
         self.m_animCtrl1 = wx.adv.AnimationCtrl( self, wx.ID_ANY, wx.adv.NullAnimation, wx.DefaultPosition, wx.DefaultSize, wx.adv.AC_DEFAULT_STYLE ) 
         bSizer3.Add( self.m_animCtrl1, 1, wx.ALL|wx.EXPAND, 5 )        
         bSizer2.Add( bSizer3, 9, wx.EXPAND, 5 )
@@ -51,7 +42,6 @@ class Fatigue_detecting(wx.Frame):
         gSizer1.Add( self.camera_button1, 0, wx.ALL, 5 )
         self.vedio_button2 = wx.Button( sbSizer2.GetStaticBox(), wx.ID_ANY, u"打开视频文件", wx.DefaultPosition, wx.Size( 90,25 ), 0 )
         gSizer1.Add( self.vedio_button2, 0, wx.ALL, 5 )
-
         self.off_button3 = wx.Button( sbSizer2.GetStaticBox(), wx.ID_ANY, u"暂停", wx.DefaultPosition, wx.Size( 90,25 ), 0 )
         gSizer1.Add( self.off_button3, 0, wx.ALL, 5 )
         sbSizer2.Add( gSizer1, 1, wx.EXPAND, 5 )
@@ -112,61 +102,35 @@ class Fatigue_detecting(wx.Frame):
         self.SetSizer( bSizer1 )  
         self.Layout()
         self.Centre( wx.BOTH )
-        
-        # Connect Events
-        self.m_choice1.Bind( wx.EVT_CHOICE, self.cameraid_choice )#绑定事件
-        self.camera_button1.Bind( wx.EVT_BUTTON, self.camera_on )#开
+        self.m_choice1.Bind( wx.EVT_CHOICE, self.cameraid_choice )
+        self.camera_button1.Bind( wx.EVT_BUTTON, self.camera_on )
         self.vedio_button2.Bind( wx.EVT_BUTTON, self.vedio_on )
-        self.off_button3.Bind( wx.EVT_BUTTON, self.off )#关
-
-        self.m_listBox2.Bind( wx.EVT_LISTBOX, self.AR_CONSEC_FRAMES )# 闪烁阈值设置
-        self.m_listBox21.Bind( wx.EVT_LISTBOX, self.OUT_AR_CONSEC_FRAMES )# 脱岗时间设置
-        
-        # 封面图片
+        self.off_button3.Bind( wx.EVT_BUTTON, self.off )
+        self.m_listBox2.Bind( wx.EVT_LISTBOX, self.AR_CONSEC_FRAMES )
+        self.m_listBox21.Bind( wx.EVT_LISTBOX, self.OUT_AR_CONSEC_FRAMES )
         self.image_cover = wx.Image(COVER, wx.BITMAP_TYPE_ANY)
-        # 显示图片在m_animCtrl1上
         self.bmp = wx.StaticBitmap(self.m_animCtrl1, -1, wx.Bitmap(self.image_cover))
-
-        # 设置窗口标题的图标
         self.icon = wx.Icon('./images/123.ico', wx.BITMAP_TYPE_ICO)
         self.SetIcon(self.icon)
-        # 系统事件
-        self.Bind(wx.EVT_CLOSE, self.OnClose)
-        
+        self.Bind(wx.EVT_CLOSE, self.OnClose)     
         print("wxpython界面初始化加载完成！")
-        
-        """参数"""
-        # 默认为摄像头0
         self.VIDEO_STREAM = 0
-        self.CAMERA_STYLE = False # False未打开摄像头，True摄像头已打开
-        # 闪烁阈值（秒）
+        self.CAMERA_STYLE = False
         self.AR_CONSEC_FRAMES_check = 3
         self.OUT_AR_CONSEC_FRAMES_check = 5
-        # 眼睛长宽比
         self.EYE_AR_THRESH = 0.2
         self.EYE_AR_CONSEC_FRAMES = self.AR_CONSEC_FRAMES_check
-        # 打哈欠长宽比
         self.MAR_THRESH = 0.5
         self.MOUTH_AR_CONSEC_FRAMES = self.AR_CONSEC_FRAMES_check
-        # 瞌睡点头
         self.HAR_THRESH = 0.3
         self.NOD_AR_CONSEC_FRAMES = self.AR_CONSEC_FRAMES_check
-        
-        """计数"""
-        # 初始化帧计数器和眨眼总数
         self.COUNTER = 0
         self.TOTAL = 0
-        # 初始化帧计数器和打哈欠总数
         self.mCOUNTER = 0
         self.mTOTAL = 0
-        # 初始化帧计数器和点头总数
         self.hCOUNTER = 0
         self.hTOTAL = 0
-        # 离职时间长度
         self.oCOUNTER = 0
-
-        """姿态"""
-        # 世界坐标系(UVW)：填写3D参考点，该模型参考http://aifi.isr.uc.pt/Downloads/OpenGL/glAnthropometric3DModel.cpp
         self.object_pts = np.float32([[6.825897, 6.760612, 4.402142],  #33左眉左上角
                                  [1.330353, 7.122144, 6.903745],  #29左眉右角
                                  [-1.330353, 7.122144, 6.903745], #34右眉左角
@@ -181,19 +145,12 @@ class Fatigue_detecting(wx.Frame):
                                  [-2.774015, -2.080775, 5.048531],#39嘴右上角
                                  [0.000000, -3.116408, 6.097667], #45嘴中央下角
                                  [0.000000, -7.415691, 4.070434]])#6下巴角
-
-        # 相机坐标系(XYZ)：添加相机内参
         self.K = [6.5308391993466671e+002, 0.0, 3.1950000000000000e+002,
                  0.0, 6.5308391993466671e+002, 2.3950000000000000e+002,
-                 0.0, 0.0, 1.0]# 等价于矩阵[fx, 0, cx; 0, fy, cy; 0, 0, 1]
-        # 图像中心坐标系(uv)：相机畸变参数[k1, k2, p1, p2, k3]
+                 0.0, 0.0, 1.0]
         self.D = [7.0834633684407095e-002, 6.9140193737175351e-002, 0.0, 0.0, -1.3073460323689292e+000]
-
-        # 像素坐标系(xy)：填写凸轮的本征和畸变系数
         self.cam_matrix = np.array(self.K).reshape(3, 3).astype(np.float32)
         self.dist_coeffs = np.array(self.D).reshape(5, 1).astype(np.float32)
-
-        # 重新投影3D点的世界坐标轴以验证结果姿势
         self.reprojectsrc = np.float32([[10.0, 10.0, 10.0],
                                        [10.0, 10.0, -10.0],
                                        [10.0, -10.0, -10.0],
@@ -202,7 +159,6 @@ class Fatigue_detecting(wx.Frame):
                                        [-10.0, 10.0, -10.0],
                                        [-10.0, -10.0, -10.0],
                                        [-10.0, -10.0, 10.0]])
-        # 绘制正方体12轴
         self.line_pairs = [[0, 1], [1, 2], [2, 3], [3, 0],
                           [4, 5], [5, 6], [6, 7], [7, 4],
                           [0, 4], [1, 5], [2, 6], [3, 7]]
@@ -211,49 +167,29 @@ class Fatigue_detecting(wx.Frame):
     def __del__( self ):
         pass
 
-    def get_head_pose(self,shape):# 头部姿态估计
-        # （像素坐标集合）填写2D参考点，注释遵循https://ibug.doc.ic.ac.uk/resources/300-W/
-        # 17左眉左上角/21左眉右角/22右眉左上角/26右眉右上角/36左眼左上角/39左眼右上角/42右眼左上角/
-        # 45右眼右上角/31鼻子左上角/35鼻子右上角/48左上角/54嘴右上角/57嘴中央下角/8下巴角
+    def get_head_pose(self,shape):
         image_pts = np.float32([shape[17], shape[21], shape[22], shape[26], shape[36],
                                 shape[39], shape[42], shape[45], shape[31], shape[35],
                                 shape[48], shape[54], shape[57], shape[8]])
-        # solvePnP计算姿势——求解旋转和平移矩阵：
-        # rotation_vec表示旋转矩阵，translation_vec表示平移矩阵，cam_matrix与K矩阵对应，dist_coeffs与D矩阵对应。
         _, rotation_vec, translation_vec = cv2.solvePnP(self.object_pts, image_pts, self.cam_matrix, self.dist_coeffs)
-        # projectPoints重新投影误差：原2d点和重投影2d点的距离（输入3d点、相机内参、相机畸变、r、t，输出重投影2d点）
         reprojectdst, _ = cv2.projectPoints(self.reprojectsrc, rotation_vec, translation_vec, self.cam_matrix,self.dist_coeffs)
-        reprojectdst = tuple(map(tuple, reprojectdst.reshape(8, 2)))# 以8行2列显示
-
-        # 计算欧拉角calc euler angle
-        # 参考https://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html#decomposeprojectionmatrix
-        rotation_mat, _ = cv2.Rodrigues(rotation_vec)#罗德里格斯公式（将旋转矩阵转换为旋转向量）
-        pose_mat = cv2.hconcat((rotation_mat, translation_vec))# 水平拼接，vconcat垂直拼接
-        # decomposeProjectionMatrix将投影矩阵分解为旋转矩阵和相机矩阵
+        reprojectdst = tuple(map(tuple, reprojectdst.reshape(8, 2)))
+        rotation_mat, _ = cv2.Rodrigues(rotation_vec)
+        pose_mat = cv2.hconcat((rotation_mat, translation_vec))
         _, _, _, _, _, _, euler_angle = cv2.decomposeProjectionMatrix(pose_mat)
-
         pitch, yaw, roll = [math.radians(_) for _ in euler_angle]
-
         pitch = math.degrees(math.asin(math.sin(pitch)))
         roll = -math.degrees(math.asin(math.sin(roll)))
         yaw = math.degrees(math.asin(math.sin(yaw)))
         #print('pitch:{}, yaw:{}, roll:{}'.format(pitch, yaw, roll))
-
-        return reprojectdst, euler_angle# 投影误差，欧拉角
-    
+        return reprojectdst, euler_angle
     def eye_aspect_ratio(self,eye):
-        # 垂直眼标志（X，Y）坐标
-        A = dist.euclidean(eye[1], eye[5])# 计算两个集合之间的欧式距离
+        A = dist.euclidean(eye[1], eye[5])
         B = dist.euclidean(eye[2], eye[4])
-        # 计算水平之间的欧几里得距离
-        # 水平眼标志（X，Y）坐标
         C = dist.euclidean(eye[0], eye[3])
-        # 眼睛长宽比的计算
         ear = (A + B) / (2.0 * C)
-        # 返回眼睛的长宽比
         return ear
-
-    def mouth_aspect_ratio(self,mouth):# 嘴部
+    def mouth_aspect_ratio(self,mouth):
         A = np.linalg.norm(mouth[2] - mouth[9])  # 51, 59
         B = np.linalg.norm(mouth[4] - mouth[7])  # 53, 57
         C = np.linalg.norm(mouth[0] - mouth[6])  # 49, 55
@@ -262,59 +198,32 @@ class Fatigue_detecting(wx.Frame):
 
 
     def _learning_face(self,event):
-        """dlib的初始化调用"""
-        # 使用人脸检测器get_frontal_face_detector
         self.detector = dlib.get_frontal_face_detector()
-        # dlib的68点模型，使用作者训练好的特征预测器
-        self.predictor = dlib.shape_predictor("D:/myworkspace/JupyterNotebook/fatigue_detecting/model/shape_predictor_68_face_landmarks.dat")
+        self.predictor = dlib.shape_predictor("./model/shape_predictor_68_face_landmarks.dat")
         self.m_textCtrl3.AppendText(u"加载模型成功!!!\n")
-        # 分别获取左右眼面部标志的索引
         (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
         (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
         (mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
-        
-        #建cv2摄像头对象，这里使用电脑自带摄像头，如果接了外部摄像头，则自动切换到外部摄像头
         self.cap = cv2.VideoCapture(self.VIDEO_STREAM)
-        
-        if self.cap.isOpened()==True:#  返回true/false 检查初始化是否成功
+        if self.cap.isOpened()==True:
             self.CAMERA_STYLE = True
             self.m_textCtrl3.AppendText(u"打开摄像头成功!!!\n")
         else:
             self.m_textCtrl3.AppendText(u"摄像头打开失败!!!\n")
-            #显示封面图
             self.bmp.SetBitmap(wx.Bitmap(self.image_cover))
-        # 成功打开视频，循环读取视频流
         while(self.cap.isOpened()):
-            # cap.read()
-            # 返回两个值：
-            #    一个布尔值true/false，用来判断读取视频是否成功/是否到视频末尾
-            #    图像对象，图像的三维矩阵
             flag, im_rd = self.cap.read()
-            # 取灰度
             img_gray = cv2.cvtColor(im_rd, cv2.COLOR_RGB2GRAY)
-            
-            # 使用人脸检测器检测每一帧图像中的人脸。并返回人脸数faces
             faces = self.detector(img_gray, 0)
-            # 如果检测到人脸
             if(len(faces)!=0):
-                # enumerate方法同时返回数据对象的索引和数据，k为索引，d为faces中的对象
                 for k, d in enumerate(faces):
-                    # 用红色矩形框出人脸
                     cv2.rectangle(im_rd, (d.left(), d.top()), (d.right(), d.bottom()), (0, 0, 255),1)
-                    # 使用预测器得到68点数据的坐标
                     shape = self.predictor(im_rd, d)
-                    # 圆圈显示每个特征点
                     for i in range(68):
                         cv2.circle(im_rd, (shape.part(i).x, shape.part(i).y), 2, (0, 255, 0), -1, 8)
-                    # 将脸部特征信息转换为数组array的格式
                     shape = face_utils.shape_to_np(shape)
-                    """
-                    打哈欠
-                    """
                     if self.yawn_checkBox1.GetValue()== True:
-                        # 嘴巴坐标
                         mouth = shape[mStart:mEnd]        
-                        # 打哈欠
                         mar = self.mouth_aspect_ratio(mouth)
                         # 使用cv2.convexHull获得凸包位置，使用drawContours画出轮廓位置进行画图操作
                         mouthHull = cv2.convexHull(mouth)
